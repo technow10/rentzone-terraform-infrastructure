c1255b20ca0a811bf167583c516ea78ccc4d9f0c
Techaddict <kindheart9ja@gmail.com>
2024-04-19 16:30:51 -0500

    Create an rds module

--- main.tf
+++ main.tf
@@ -19,3 +19,49 @@ module "vpc" {
   private_data_subnet_data1_cidr = var.private_data_subnet_data1_cidr
   private_data_subnet_data2_cidr = var.private_data_subnet_data2_cidr
 }
+
+# Create a NAT Gateway repository
+
+module "Nat-gw" {
+  source       = "git@github.com:technow10/rentzone-terraform-modules.git//Nat_Gateway"
+  project_name = local.project_name
+  environment  = local.environment
+
+  # VPC s for NAT Gateway modules
+  vpc_id                     = module.vpc.vpc_id
+  internet_gateway           = module.vpc.internet_gateway
+  public_subnet_az1_id       = module.vpc.public_subnet_az1_id
+  public_subnet_az2_id       = module.vpc.public_subnet_az2_id
+  private_app_subnet_az1_id  = module.vpc.private_app_subnet_az1_id
+  private_app_subnet_az2_id  = module.vpc.private_app_subnet_az2_id
+  private_data_subnet_az1_id = module.vpc.private_data_subnet_az1_id
+  private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
+
+}
+
+# Create security group
+
+module "security-group" {
+  source       = "git@github.com:technow10/rentzone-terraform-modules.git//security-group"
+  project_name = local.project_name
+  environment  = local.environment
+  vpc_id       = module.vpc.vpc_id
+  ssh-ip       = var.ssh-ip
+}
+
+#create RDS modules
+
+# Create RDS variable
+module "rds" {
+  source                       = "git@github.com:technow10/rentzone-terraform-modules.git//rds"
+  project_name                 = local.project_name
+  environment                  = local.environment
+  private_data_subnet_az1_id   = module.vpc.private_data_subnet_az1_id
+  private_data_subnet_az2_id   = module.vpc.private_data_subnet_az2_id
+  database_snapshot_identifier = var.vpc.database_snapshot_identifier
+  database_instance_class      = var.vpc.database_instance_class
+  availability_zone_1          = module.vpc.availability_zone_1
+  db_instance_identifier       = var.db_instance_identifier
+  multi_az_deployment          = var.multi_az_deployment
+  database_security_group_id   = module.security_group.database_security_group_id
+}
--- variable.tf
+++ variable.tf
@@ -11,3 +11,17 @@ variable "private_app_subnet_az1_cidr" {}
 variable "private_app_subnet_az2_cidr" {}
 variable "private_data_subnet_data1_cidr" {}
 variable "private_data_subnet_data2_cidr" {}
+
+#create security groups variable in modules
+variable "ssh-ip" {}
+
+# Create RDS variable
+variable "private_data_subnet_az1_id" {}
+variable "private_data_subnet_az2_id" {}
+variable "database_snapshot_identifier" {}
+variable "database_instance_class" {}
+variable "availability_zone_1" {}
+variable "db_instance_identifier" {}
+variable "multi_az_deployment" {}
+variable "database_security_group_id" {
+}
