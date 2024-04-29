d982dabcafb65fd809fe5b31b245e30e664be998
Techaddict <kindheart9ja@gmail.com>
2024-04-29 11:18:36 -0500

    update backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         = "terraform-remote-statefile-bucket"
+    bucket         = "terraform-remote-statefile-rentzone"
     key            = "terraform-module/rentzone/terraform-tfstate"
     region         = "us-east-1"
     profile        = "cloud-admn-user"
